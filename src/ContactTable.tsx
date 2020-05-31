6107b7403b1fab292e35b5a9fddb59877321314e
Aiden Scott <aidenscott12@googlemail.com>
2020-05-31 14:21:09 +0100

    create contact table

---
+++ src/ContactTable.tsx
@@ -0,0 +1,33 @@
+import React from "react"
+import { Contact } from "./model/Contact"
+
+interface ContactProps {
+  contact: Contact
+}
+
+const ContactRow = ({ contact }: ContactProps) => (
+  <tr>
+    <td>{contact.name}</td>
+    <td>{contact.address}</td>
+    <td>{contact.phoneNumber}</td>
+  </tr>
+)
+
+interface ContactsTableProps {
+  contacts: Contact[]
+}
+const ContactsTable = ({ contacts }: ContactsTableProps) =>
+  <table>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Address</th>
+        <th>Number</th>
+      </tr>
+    </thead>
+    <tbody>
+      {contacts.map((c, i) => <ContactRow contact={c} key={i} />)}
+    </tbody>
+  </table>
+
+export default ContactsTable
